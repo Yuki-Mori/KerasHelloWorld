9fd0c068ad08cb41c00662b9fa8d554b2a4ecd35
Yuki-Mori <yuki7291@pluslab.org>
2018-08-27 16:08:12 +0900

    全部打った

--- hello.py
+++ hello.py
@@ -6,6 +6,10 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout
 from keras.optimizers import RMSprop
 
+batch_size = 128
+num_classes = 10
+epochs = 20
+
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 x_train = x_train.reshape(60000, 784)
@@ -19,4 +23,30 @@ print(x_test.shape[0], 'test samples')
 print('enter to continue...')
 input()
 
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
 
+model = Sequential()
+model.add(Dense(512, activation='relu', input_shape=(784,)))
+model.add(Dropout(0.2))
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.summary()
+input('enter to continue...')
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=RMSprop(),
+              metrics=['accuracy'])
+
+history = model.fit(x_train, y_train,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=1,
+                    validation_data=(x_test, y_test))
+
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss: ', score[0])
+print('Test accuracy: ', score[1])
