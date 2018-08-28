65e02ae1e15121e430623299cfd9954fe5648ac6
Yuki-Mori <yuki7291@pluslab.org>
2018-08-28 15:11:31 +0900

    add create_model method

--- dog_and_cat.py
+++ dog_and_cat.py
@@ -21,7 +21,7 @@ def get_dataset(dirname):
 
     X = []
     Y = []
-    
+
     for filename in dog_files:
         img = load_img(filename, target_size=TARGET_SIZE)
         array = img_to_array(img) / 255
@@ -35,3 +35,22 @@ def get_dataset(dirname):
         Y.append([1,0])
 
     return (X, Y)
+
+def create_model():
+    model = Sequential()
+    model.add(Conv2D(32,(3,3), input_shape=(TARGET_SIZE[0],TARGET_SIZE[1], 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+
+    model.add(Conv2D(64,(3,3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+
+    model.add(Flatten())
+    model.add(Dense(64))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(2))
+    model.add(Activation('softmax'))
+
+    return model
